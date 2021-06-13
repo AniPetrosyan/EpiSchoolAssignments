5fb1d5af301d131e5993f723f5fc898d5d8a69a5
AniPetrosyan <ani.petrosyan.y@tumo.org>
2021-06-13 11:32:43 +0400

    primes_below

---
+++ primesBelowN.py
@@ -0,0 +1,10 @@
+def primes_below(n):
+    numbers = set(range(n, 1, -1))
+    primes = []
+    while numbers:
+        p = numbers.pop()
+        primes.append(p)
+        numbers.difference_update(set(range(p*2, n+1, p)))
+    return primes
+
+print(primes_below(2))
